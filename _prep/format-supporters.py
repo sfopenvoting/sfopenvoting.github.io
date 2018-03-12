27484844f52ec198cfc2eaa885fb5f6c5df9306d
Chris Jerdonek <chris.jerdonek@gmail.com>
2018-03-12 04:38:22 -0700

    Add _data directory.

--- _prep/format-supporters.py
+++ _prep/format-supporters.py
@@ -2,10 +2,12 @@
 This script requires Python 3.6+.
 """
 
+import json
 from pathlib import Path
 
 
-SUPPORTERS_PATH = '_data/sf-individuals.txt'
+SUPPORTERS_JSON_PATH = '_data/supporters.json'
+SUPPORTERS_PATH = '_prep/sf-individuals.txt'
 
 def main():
     path = Path(SUPPORTERS_PATH)
@@ -13,6 +15,7 @@ def main():
     lines = text.splitlines()
     print(f'read {len(lines)} supporters')
 
+    data = {'sf-residents': lines}
     lines = sorted(lines)
     # Format for Markdown.
     lines = [f'1. {line}' for line in lines]
@@ -21,6 +24,9 @@ def main():
     temp_path = Path('temp.md')
     print(f'writing to: {temp_path}')
     temp_path.write_text(markdown)
+
+    with open(SUPPORTERS_JSON_PATH, mode='w') as f:
+        json.dump(data, f, indent=4)
 
 
 if __name__ == '__main__':
